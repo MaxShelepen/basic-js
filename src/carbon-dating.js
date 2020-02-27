b697d0a8c6d07fbb0de32232471c8f8f27b190ca
maxshelepen <maxshelepen25@gmail.com>
2020-02-27 23:46:54 +0300

    feat: complited hanoi tower

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -11,10 +11,10 @@ module.exports = function dateSample( sampleActivity ) {
   }
   let parse = parseFloat(sampleActivity);
   if (isNaN(parse) || parse > MODERN_ACTIVITY || parse <= 0) return false;
-  
+
     let k =  0.693/HALF_LIFE_PERIOD;
    
  
-  return Math.ceil((Math.log(MODERN_ACTIVITY/parse)/k))
+  return Math.ceil((Math.log(MODERN_ACTIVITY/parse)/k));
   
 };
